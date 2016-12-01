aad3e30da354061a4a53e12e5cfa28d400bc886a
BiizmarckRodriguez <daemonspade114@gmail.com>
2016-12-01 09:48:17 -0600

    Update Actividad1.py

--- Laboratorio3/Actividad1.py
+++ Laboratorio3/Actividad1.py
@@ -2,16 +2,18 @@
 #Laboratorio de computo 3
 #29/11/2016
 #Actividad 1
-from math import sin,cos,sqrt,asin,acos,pi
+import math as r
 def cos(angulo):
     return r.cos(r.radians(angulo))
-def sin(angulo):
+def sin(ang):
     return r.sin(r.radians(angulo))
 def acos(angulo):
     return r.acos(r.radians(angulo))
-    x1=input ("Latitud 1:")
-    y1=input ("longitud 1:")
-    x2=input ("Latitud 2:")
-    y2=input ("longitud 2:")
-def distancia(puntosgeograficos):
-    distancia=6371*acos(sin(x1)sin(x2)+cos(x1)cos(x2)cos(y1-y2))
+x1=float(input ("Introduzca latitud: "))
+y1=float(input ("Introduzca longitud: "))
+x2=float(input ("Introduzca latitud2: "))
+y2=float(input ("Introduzca longitud2: "))
+def distancia(x1,y1,x2,y2):
+    a=6371.01*r.acos(r.sin(x1)*r.sin(x2)*r.cos(x1)*r.cos(x2)*r.cos(y1-y2))
+    print a
+
